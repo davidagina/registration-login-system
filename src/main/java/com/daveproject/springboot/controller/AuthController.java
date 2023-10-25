d3d7f1a25398e8024b6949ddaf835ec54963c036
davidagina <davidagina64@gmail.com>
2023-10-25 17:48:48 +0100

    Added login method

--- src/main/java/com/daveproject/springboot/controller/AuthController.java
+++ src/main/java/com/daveproject/springboot/controller/AuthController.java
@@ -27,6 +27,11 @@ public class AuthController {
         return "index";
     }
 
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
     @GetMapping("/register")
     public String showRegistrationForm(Model model){
         UserDto user = new UserDto();
