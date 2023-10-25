c8735b897b182cb41ed4443861bab3d9ab170678
davidagina <davidagina64@gmail.com>
2023-10-25 17:50:56 +0100

    Configured custom login page

---
+++ src/main/java/com/daveproject/springboot/config/SpringSecurity.java
@@ -0,0 +1,30 @@
+package com.daveproject.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    // configure SecurityFilterChain
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin(
+                        form ->  form.loginPage("/login").loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/users").permitAll()
+
+                );
+        return http.build();
+    }
+}
