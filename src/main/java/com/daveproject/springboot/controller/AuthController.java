8db654ae521dd23c08c212a7fcd3af58ce271753
davidagina <davidagina64@gmail.com>
2023-10-25 06:58:11 +0100

    Added validation

--- src/main/java/com/daveproject/springboot/controller/AuthController.java
+++ src/main/java/com/daveproject/springboot/controller/AuthController.java
@@ -1,12 +1,24 @@
 package com.daveproject.springboot.controller;
 
 import com.daveproject.springboot.dto.UserDto;
+import com.daveproject.springboot.entity.User;
+import com.daveproject.springboot.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService){
+        this.userService = userService;
+    }
 
     @GetMapping("/index")
     public String home(){
@@ -18,5 +30,26 @@ public class AuthController {
         UserDto user = new UserDto();
         model.addAttribute("user", user);
         return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String saveUser(@Valid @ModelAttribute("user") UserDto userDto,
+                           BindingResult result,
+                           Model model){
+
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "Email address is registered wih an account");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
     }
 }
