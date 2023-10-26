aedcb276bf9ae9e8e3051cbfb7f074be6e4ece4d
davidagina <davidagina64@gmail.com>
2023-10-26 04:20:28 +0100

    Added CustomerUserDetailsService class

---
+++ src/main/java/com/daveproject/springboot/security/CustomerUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.daveproject.springboot.security;
+
+import com.daveproject.springboot.entity.User;
+import com.daveproject.springboot.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user != null){
+            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                    user.getPassword(), user.getRoles().stream().map((role) ->
+            new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList()));
+        } else
+        {
+            throw new UsernameNotFoundException("Invalid email or password");
+
+    }
+}
