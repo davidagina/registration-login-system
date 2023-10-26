135b23f024cc829ef12bffb571da58d136966696
davidagina <davidagina64@gmail.com>
2023-10-26 04:18:30 +0100

    encrypt database password with spring security (passwordEncoder)

--- src/main/java/com/daveproject/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/daveproject/springboot/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.daveproject.springboot.entity.User;
 import com.daveproject.springboot.repository.RoleRepository;
 import com.daveproject.springboot.repository.UserRepository;
 import com.daveproject.springboot.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -17,10 +18,13 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository){
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     @Override
     public void saveUser(UserDto userDto) {
@@ -28,7 +32,7 @@ public class UserServiceImpl implements UserService {
         user.setName(userDto.getFirstName() + " " + userDto.getLastName());
         user.setEmail(userDto.getEmail());
         // encrypt password using spring security
-        user.setPassword(userDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
         Role role = roleRepository.findByName("ROLE_ADMIN");
         if (role == null){
